0cb6f4054d79bfdd86c81dd8a6e8520d5aee0fd4
TFor <tforren@udel.edu>
2016-11-09 13:43:14 -0500

    initialized file2nx script

---
+++ file2nx.py
@@ -0,0 +1,13 @@
+import networkx as nx
+import sys
+
+socialGraph = nx.Graph()
+
+f = open(sys.argv[1])
+SocialGraphFile = f.readlines()
+f.close()
+
+def parseFileLines(fileLines):
+	return false
+
+
